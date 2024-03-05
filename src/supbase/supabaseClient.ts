92385e0b85f4fab65eff692ebd251724fd135058
Trinh Nguyen <trinhnguyen@Trinhs-MBP.cable.rcn.com>
2024-03-05 09:32:59 -0500

    Add supabase URL and Anon key to supabaseClient.ts

--- src/supbase/supabaseClient.ts
+++ src/supbase/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://iaqrbclvxokopyeugxmf.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhcXJiY2x2eG9rb3B5ZXVneG1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NDMxNjUsImV4cCI6MjAyMzAxOTE2NX0.ki-0qqRjR9hn13_ppL8YLfCfmTvSEE3B_59iwLDpo8k'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// import.meta.env
